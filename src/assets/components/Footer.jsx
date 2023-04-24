15361900a2a6b3a922ea026614406de540e0d7a3
Elsa Letallieur <elsa.c.letallieur@gmail.com>
2023-04-24 16:41:19 +0200

    responsive last save

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = (props) => {
   return (
     <p className="footer">
-      Made with<span>React</span>at <span>Le Reacteur </span>By{" "}
+      Made with<span>React</span>at <span>Le Reacteur </span>By
       <span>{props.name}</span>
     </p>
   );
